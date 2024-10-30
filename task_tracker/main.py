cb4845b794dec8a9cc30cf699cc18857c04ea832
Israel Arantes <israelarantes.m@gmail.com>
2024-10-29 22:40:00 -0300

    + organized into class + handling error from exiting the app + fixed how to exit without having to write the command "taskctl"

--- task_tracker/main.py
+++ task_tracker/main.py
@@ -2,19 +2,33 @@ import argparse
 import time
 
 parser = argparse.ArgumentParser(prog='taskctl', description='Creates tasks')
+parser.add_argument('taskctl', help='Digite o comprimento e largura')
 parser.add_argument('-l','--largura')
 parser.add_argument('-c', '--comprimento')
 
+class NewParser():
+    def task_args(self):
+        while True:
+            text = input('$ ').lower()
+
+            if text in ['exit', 'stop']:
+                print('Exiting task tracker!')
+                time.sleep(0.5)
+                break
+            elif not text.startswith('taskctl'):
+                print('Command not recognized or missing: taskctl')
+            else:
+                try:
+                    args = parser.parse_args(text.split())
+                    print(f'Largura = {args.largura}, comprimento = {args.comprimento}')
+                except SystemExit:
+                    pass
+
+
 def main():
-    while True:
-        text = input('$ ').lower()
-        if text != 'exit':
-            args = parser.parse_args(text.split())
-            print(args.comprimento)
-        else:
-            print("Exiting the Task Tracker")
-            time.sleep(1)
-            break
+
+    result = NewParser()
+    result.task_args()
 
 if __name__ == '__main__':
     main()
